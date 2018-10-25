c6e820cd230d86c461577bd8b2fcb42f405c7f5e
Allen Shen <ashen1@stuy.edu>
2018-10-25 11:26:43 -0400

    second testing round

--- Driver.java
+++ Driver.java
@@ -1,8 +1,11 @@
 public class Driver{
   public static void main(String[] args) {
-    SuperArray a = new SuperArray()
+    SuperArray a = new SuperArray();
     System.out.println(a);
     System.out.println(a.size());
-    
+    a.clear();
+    System.out.println(a);
+    System.out.println(a.isEmpty());
+    System.out.println(a.toStringDebug());
   }
 }
